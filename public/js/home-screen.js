e2599cd200027ba0e20a3c68df4756d0110d1b64
CHinX2 <iris870425@gmail.com>
2019-06-24 05:03:01 +0800

    home css

--- public/js/home-screen.js
+++ public/js/home-screen.js
@@ -8,23 +8,11 @@ class HomeScreen {
     this.containerElement.classList.remove('inactive');
 
     // add button
-
-    for (let i = 0; i < FLASHCARD_DECKS.length; i++)
-    {
-      var btn = document.createElement('div');
-      btn.textContent =  FLASHCARD_DECKS[i].title;
-      btn.id = i;
-      btn.addEventListener('click', this._gotoMain);
-      document.querySelector('#choices').appendChild(btn);
-    }
+    var btn = document.querySelector('#start');
+    btn.addEventListener('click', this._gotoMain);
   }
 
   hide() {
-    // remove button
-    var list = document.querySelector('#choices');
-    while(list.hasChildNodes()) {
-      list.removeChild(list.firstChild);
-    }
     this.containerElement.classList.add('inactive');
   }
 
